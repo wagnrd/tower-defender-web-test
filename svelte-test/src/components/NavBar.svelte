2f40ab32193924c7fc31eaa0ed9c31d7dbc5bb7d
wagnrd <projects@wagnrd.de>
2022-09-18 22:11:35 +0200

    adds wip nav bar

---
+++ svelte-test/src/components/NavBar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+
+</script>
+
+<div class="nav-bar-shadow">
+    <div class="nav-bar">
+
+    </div>
+</div>
+
+<style>
+    .nav-bar {
+        width: 70vw;
+        height: 6vw;
+        top: 0;
+        position: fixed;
+        background-color: var(--base-color);
+        clip-path: polygon(
+                0% 0%,
+                90% 0%,
+                100% 100%,
+                0% 100%
+        );
+        z-index: 100;
+    }
+
+    .nav-bar-shadow {
+        filter: drop-shadow(10px 10px 5px rgba(0, 0, 0, 0.5));
+    }
+</style>
