50f361e5d8173862bd253fc95dde75a806661d87
wagnrd <projects@wagnrd.de>
2022-10-11 23:37:09 +0200

    adds wip splash screen

--- svelte-test/src/pages/Home/Home.svelte
+++ svelte-test/src/pages/Home/Home.svelte
@@ -4,47 +4,63 @@
     import Section from "../../components/Section.svelte";
     import NewsCarousel from "./components/NewsCarousel/NewsCarousel.svelte";
     import KeyFeature from "./components/KeyFeature.svelte";
+    import Splash from "./components/Splash.svelte";
 </script>
 
+<Splash/>
 <div id="page">
-    <Section title="LATEST NEWS" right>
-        <div id="news-carousel">
-            <NewsCarousel/>
-        </div>
-        <img src={armouredTruckImage} id="latest-news-section-image" alt="Armoured truck"/>
-    </Section>
+    <div>
+        <Section title="LATEST NEWS" right>
+            <div id="news-carousel">
+                <NewsCarousel/>
+            </div>
+            <img src={armouredTruckImage} id="latest-news-section-image" alt="Armoured truck"/>
+        </Section>
 
-    <Section title="KEY FEATURES">
-        <img src={bigGun2Image} id="key-features-section-image" alt="big gun"/>
-        <div id="key-features">
-            <KeyFeature title="Campaign">
-                Play a campaign with dozens of exciting scenarios. The further you progress through the campaign the
-                harder the challenges will get!<br/>
-                Try and find the perfect moves to beat the incoming armies that want to
-                destroy your nexus. Set your personal best times and compete against others on a global leaderboard.
-            </KeyFeature>
-            <KeyFeature title="Endless Mode">
-                No story, no limitations, just pure tower defense!<br/>
-                If you really want to push your boundaries you can set your own scenario and go as long as you can last.<br/>
-                With personal best times and global leaderboards to open the competition!
-            </KeyFeature>
-            <KeyFeature title="Mazing">
-                You can position your turrets where you want! Employ your own building style with turret mazes for the
-                incoming enemies.<br/>
-                Only the resources are your limits! Don't let the armies go straight to your nexus. Maze them through a
-                jungle of walls, turrets and mines to keep them from your base as long as possible!
-            </KeyFeature>
-            <KeyFeature title="Map Editor & Sharing Platform" last>
-                Tired of the standard campaign maps? Just create your own and share it with others!<br/>
-                Or download some awesome custom maps from other players on the game internal sharing platform.
-            </KeyFeature>
-        </div>
-    </Section>
+        <Section title="KEY FEATURES">
+            <img src={bigGun2Image} id="key-features-section-image" alt="big gun"/>
+            <div id="key-features">
+                <KeyFeature title="Campaign">
+                    Play a campaign with dozens of exciting scenarios. The further you progress through the campaign
+                    the
+                    harder the challenges will get!<br/>
+                    Try and find the perfect moves to beat the incoming armies that want to
+                    destroy your nexus. Set your personal best times and compete against others on a global
+                    leaderboard.
+                </KeyFeature>
+                <KeyFeature title="Endless Mode">
+                    No story, no limitations, just pure tower defense!<br/>
+                    If you really want to push your boundaries you can set your own scenario and go as long as you
+                    can
+                    last.<br/>
+                    With personal best times and global leaderboards to open the competition!
+                </KeyFeature>
+                <KeyFeature title="Mazing">
+                    You can position your turrets where you want! Employ your own building style with turret mazes
+                    for
+                    the
+                    incoming enemies.<br/>
+                    Only the resources are your limits! Don't let the armies go straight to your nexus. Maze them
+                    through a
+                    jungle of walls, turrets and mines to keep them from your base as long as possible!
+                </KeyFeature>
+                <KeyFeature title="Map Editor & Sharing Platform" last>
+                    Tired of the standard campaign maps? Just create your own and share it with others!<br/>
+                    Or download some awesome custom maps from other players on the game internal sharing platform.
+                </KeyFeature>
+            </div>
+        </Section>
+    </div>
 </div>
 
 <style>
     #page {
-        margin-top: 10rem;
+        margin-top: 5rem;
+    }
+
+    .content {
+        position: fixed;
+        overflow-y: hidden;
     }
 
     #news-carousel {
---
+++ svelte-test/src/pages/Home/components/Splash.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    const percentageScrolledFadeThreshold = 0.3
+    let y = 0;
+
+    const updateSplash = (newY: number) => {
+        console.log(newY);
+        const elem = document.getElementById("splash");
+
+        if (!elem)
+            return;
+
+        const percentageScrolled = 1 / window.innerHeight.valueOf() * newY;
+
+        if (percentageScrolled > percentageScrolledFadeThreshold)
+            elem.style.opacity = (1 - ((percentageScrolled - percentageScrolledFadeThreshold) * 2)).toString(10);
+    }
+    $: updateSplash(y);
+</script>
+
+<svelte:window bind:scrollY={y}/>
+<div id="splash">
+</div>
+
+<style>
+    #splash {
+        width: 100vw;
+        height: 100vh;
+        z-index: 101;
+        background: url("./src/assets/splash-background.jpg") top center/cover no-repeat;
+    }
+</style>
