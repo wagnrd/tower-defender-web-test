a2ad894231977c255ce74a21982b116eab19052f
wagnrd <projects@wagnrd.de>
2022-10-11 19:04:58 +0200

    Adds real key feature texts

--- svelte-test/src/pages/Home/Home.svelte
+++ svelte-test/src/pages/Home/Home.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-    import Section from "../../components/Section.svelte";
     import armouredTruckImage from "../../assets/armoured-truck.png";
     import bigGun2Image from "../../assets/big-gun-2.png";
+    import Section from "../../components/Section.svelte";
     import NewsCarousel from "./components/NewsCarousel/NewsCarousel.svelte";
     import KeyFeature from "./components/KeyFeature.svelte";
 </script>
@@ -17,28 +17,26 @@
     <Section title="KEY FEATURES">
         <img src={bigGun2Image} id="key-features-section-image" alt="big gun"/>
         <div id="key-features">
-            <KeyFeature title="KEY FEATURE 1">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            <KeyFeature title="Campaign">
+                Play a campaign with dozens of exciting scenarios. The further you progress through the campaign the
+                harder the challenges will get!<br/>
+                Try and find the perfect moves to beat the incoming armies that want to
+                destroy your nexus. Set your personal best times and compete against others on a global leaderboard.
             </KeyFeature>
-            <KeyFeature title="KEY FEATURE 2">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-                dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-                magna aliquyam erat, sed diam voluptua.
+            <KeyFeature title="Endless Mode">
+                No story, no limitations, just pure tower defense!<br/>
+                If you really want to push your boundaries you can set your own scenario and go as long as you can last.<br/>
+                With personal best times and global leaderboards to open the competition!
             </KeyFeature>
-            <KeyFeature title="KEY FEATURE 3">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+            <KeyFeature title="Mazing">
+                You can position your turrets where you want! Employ your own building style with turret mazes for the
+                incoming enemies.<br/>
+                Only the resources are your limits! Don't let the armies go straight to your nexus. Maze them through a
+                jungle of walls, turrets and mines to keep them from your base as long as possible!
             </KeyFeature>
-            <KeyFeature title="KEY FEATURE 4" last>
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et
-                ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
-                dolor sit amet, consetetur sadipscing elitr.
+            <KeyFeature title="Map Editor & Sharing Platform" last>
+                Tired of the standard campaign maps? Just create your own and share it with others!<br/>
+                Or download some awesome custom maps from other players on the game internal sharing platform.
             </KeyFeature>
         </div>
     </Section>
