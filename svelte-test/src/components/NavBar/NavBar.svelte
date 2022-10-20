fececc8777fa85536427902ce2e685a4266c8cce
wagnrd <projects@wagnrd.de>
2022-10-21 01:57:01 +0200

    tidies up hamburger and makes nav-bar position fixed on mobile

--- svelte-test/src/components/NavBar/NavBar.svelte
+++ svelte-test/src/components/NavBar/NavBar.svelte
@@ -2,6 +2,7 @@
     import {navigate} from "svelte-routing";
     import logoDarkSmall from "../../assets/images/logo-dark-small.svg";
     import {navBarHidden} from "./components/nav-bar-store";
+    import Hamburger from "./components/Hamburger.svelte";
 
     export let homeRoute = "";
 
@@ -23,22 +24,8 @@
     </div>
     <div id="bar-ornament"></div>
     <div id="contrast-ornament">
-        <div id="hamburger" class="mobile-visible">
-            <div class="hamburger-bar">
-                <div class="triangle left"></div>
-                <div class="bar nr1"></div>
-                <div class="triangle right"></div>
-            </div>
-            <div class="hamburger-bar">
-                <div class="triangle left"></div>
-                <div class="bar nr2"></div>
-                <div class="triangle right"></div>
-            </div>
-            <div class="hamburger-bar">
-                <div class="triangle left"></div>
-                <div class="bar nr3"></div>
-                <div class="triangle right"></div>
-            </div>
+        <div class="mobile-visible">
+            <Hamburger/>
         </div>
     </div>
 </div>
@@ -112,58 +99,6 @@
                 0% 0%,
                 100% 0%,
                 50% 100%
-        );
-    }
-
-    #hamburger {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding-top: 0.3rem;
-    }
-
-    .hamburger-bar {
-        display: flex;
-        margin-top: 0.3rem;
-    }
-
-    .triangle {
-        background-color: var(--text-color);
-        width: 0.3rem;
-        height: 0.3rem;
-    }
-
-    .triangle.left {
-        clip-path: polygon(
-                0% 0%,
-                100% 0%,
-                100% 100%
-        );
-    }
-
-    .bar {
-        background-color: var(--text-color);
-        height: 0.3rem;
-        margin: 0 -1px 0 -1px;
-    }
-
-    .bar.nr1 {
-        width: 3.7rem;
-    }
-
-    .bar.nr2 {
-        width: 2.5rem;
-    }
-
-    .bar.nr3 {
-        width: 1.2rem;
-    }
-
-    .triangle.right {
-        clip-path: polygon(
-                0% 0%,
-                100% 0%,
-                0% 100%
         );
     }
 </style>
---
+++ svelte-test/src/components/NavBar/components/Hamburger.svelte
@@ -0,0 +1,71 @@
+<div id="hamburger">
+    <div class="hamburger-bar">
+        <div class="triangle left"></div>
+        <div class="bar nr1"></div>
+        <div class="triangle right"></div>
+    </div>
+    <div class="hamburger-bar">
+        <div class="triangle left"></div>
+        <div class="bar nr2"></div>
+        <div class="triangle right"></div>
+    </div>
+    <div class="hamburger-bar">
+        <div class="triangle left"></div>
+        <div class="bar nr3"></div>
+        <div class="triangle right"></div>
+    </div>
+</div>
+
+<style>
+    #hamburger {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 0.3rem;
+    }
+
+    .hamburger-bar {
+        display: flex;
+        margin-top: 0.3rem;
+    }
+
+    .triangle {
+        background-color: var(--text-color);
+        width: 0.3rem;
+        height: 0.3rem;
+    }
+
+    .triangle.left {
+        clip-path: polygon(
+                0% 0%,
+                100% 0%,
+                100% 100%
+        );
+    }
+
+    .bar {
+        background-color: var(--text-color);
+        height: 0.3rem;
+        margin: 0 -1px 0 -1px;
+    }
+
+    .bar.nr1 {
+        width: 3.7rem;
+    }
+
+    .bar.nr2 {
+        width: 2.5rem;
+    }
+
+    .bar.nr3 {
+        width: 1.2rem;
+    }
+
+    .triangle.right {
+        clip-path: polygon(
+                0% 0%,
+                100% 0%,
+                0% 100%
+        );
+    }
+</style>
