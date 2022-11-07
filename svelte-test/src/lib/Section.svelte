2411320583ef441f42b046a74fc470a9dfe53445
wagnrd <projects@wagnrd.de>
2022-11-07 01:54:53 +0100

    gives the title min-width

--- svelte-test/src/lib/Section.svelte
+++ svelte-test/src/lib/Section.svelte
@@ -16,86 +16,15 @@
     <div class="content">{title}</div>
     <div class="right"></div>
 </div>
-<div id="section" class:right={right} class:mobile={isMobile}>
-    <!--
-    <div id="background">
-        <div id="ornament"></div>
-    </div>
-    -->
-    <div>
-        <div id="content">
-            <slot/>
-        </div>
-    </div>
+<div id="section">
+    <slot/>
 </div>
 
 <style>
     #section {
         width: 100%;
         margin-bottom: 11rem;
-        display: grid;
-        grid-template-areas: "stack";
-    }
-
-    #section > * {
-        grid-area: stack;
-    }
-
-    #background {
-        filter: drop-shadow(0.45rem 0.45rem 0.2rem var(--shadow-color));
-        z-index: -2;
-    }
-
-    .right #background {
-        filter: drop-shadow(-0.45rem 0.45rem 0.2rem var(--shadow-color));
-    }
-
-    #ornament {
-        width: 73%;
-        height: 33rem;
-        background-color: var(--base-color);
-        clip-path: polygon(
-                0% 0%,
-                100% 0%,
-                0% 100%
-        );
-    }
-
-    .right #ornament {
-        margin-left: 27%;
-        clip-path: polygon(
-                100% 0%,
-                0% 0%,
-                100% 100%
-        );
-    }
-
-    .mobile #ornament {
-        width: 100vw;
-        height: 100%;
-        margin: 0;
-    }
-
-    .mobile h1 {
-        margin-left: -5vw;
-        margin-right: 0;
-    }
-
-    .mobile.right h1 {
-        margin-left: 0;
-        margin-right: -5vw;
-    }
-
-    #content {
         display: flex;
-        align-items: start;
-    }
-
-    .right #content {
-        justify-content: end;
-    }
-
-    .mobile #content {
         justify-content: center;
     }
 
@@ -119,10 +48,12 @@
 
     #title .content {
         height: 100%;
+        min-width: 12%;
         font: var(--headline-font);
         padding: 0 2rem 0 2rem;
         display: flex;
         align-items: center;
+        justify-content: center;
     }
 
     #title.mobile .content {
