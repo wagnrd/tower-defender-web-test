e445c35980079248376db4576d03419fcc0233cf
wagnrd <projects@wagnrd.de>
2022-11-14 16:25:03 +0100

    adds base nav-bar

--- react/src/lib/NavBar/NavBar.tsx
+++ react/src/lib/NavBar/NavBar.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from "react";
 import "./NavBar.css";
 import { useApp } from "../AppProvider";
+import logoDarkSmallImage from "../../assets/images/logo-dark-small.svg";
+
 
 function NavBar(): ReactElement {
     const { isMobile } = useApp();
@@ -8,7 +10,20 @@ function NavBar(): ReactElement {
     console.log(isMobile);
 
     return (
-        <></>
+        <div id="nav-bar">
+            <div id="brand" className="clickable">
+                <img src={logoDarkSmallImage} id="logo" alt="Brand logo"/>
+                <div id="name">
+                    Y.A.T.D.
+                </div>
+            </div>
+
+            <div id="buttons">
+                <slot/>
+            </div>
+            <div id="bar-ornament"></div>
+            <div id="contrast-ornament"></div>
+        </div>
     );
 }
 
